116bae0e3afa91ceef06e2d9f00c06fec1d0557e
leowe <13684843+leowe@users.noreply.github.com>
2020-02-06 18:35:52 +0100

    Setup Django project with wagtail app

---
+++ munkey/urls.py
@@ -0,0 +1,49 @@
+"""munkey URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import re_path, path, include
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib import admin
+from django.conf import settings
+import os.path
+
+from wagtail.core import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+
+urlpatterns = [
+    re_path(r'^django-admin/', admin.site.urls),
+
+    re_path(r'^admin/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+
+    # Optional URL for including your own vanilla Django urls/views
+    re_path(r'^pm/', include('paper_management.urls')),
+
+    # For anything not caught by a more specific rule above, hand over to
+    # Wagtail's serving mechanism
+    re_path(r'', include(wagtail_urls)),
+]
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    urlpatterns += staticfiles_urlpatterns() # tell gunicorn where static files are in dev mode
+    urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
+    urlpatterns += [
+        re_path(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'myapp/images/favicon.ico'))
+    ]
---
+++ paper_management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PaperManagementConfig(AppConfig):
+    name = 'paper_management'
